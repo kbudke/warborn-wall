3e0557ceb56e27f75d02de998b34c2e0d530748d
Kyle Budke <kylepbudke@gmail.com>
2017-03-19 10:43:25 -0400

    redirect

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -40,7 +40,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to :back, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
